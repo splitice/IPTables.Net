dd190fdea99ff7f4b5e9dc0a66e5422178bbd77f
splitice <mat999@gmail.com>
2014-01-26 17:47:18 +1030

    remote process handler
    next commit will move these to a sep project.

---
+++ IPTables.Net.Remote/System/SshFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using IPTables.Net.System.Interface;
+using Renci.SshNet;
+
+namespace IPTables.Net.Remote.System
+{
+    class SshFactory
+    {
+        private readonly SshClient _connection;
+
+        public SshFactory(String host, String username, String password)
+        {
+            _connection = new SshClient(host, username, password);
+            _connection.Connect();
+        }
+        public ISystemProcess StartProcess(String command, String arguments)
+        {
+            return Remote.SshProcess.Start(new ProcessStartInfo(command, arguments), _connection);
+        }
+    }
+}
