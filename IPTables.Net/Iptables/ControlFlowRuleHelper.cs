2612619e4020a54a3236e503f7dd83beb0021df0
splitice <mat999@gmail.com>
2014-02-09 23:27:21 +1100

    helpers should be public

--- IPTables.Net/Iptables/ControlFlowRuleHelper.cs
+++ IPTables.Net/Iptables/ControlFlowRuleHelper.cs
@@ -9,26 +9,26 @@ namespace IPTables.Net.Iptables
 {
     public static class ControlFlowRuleHelper
     {
-        static IpTablesRule CreateJump(String chain, ISystemFactory system)
+        public static IpTablesRule CreateJump(String chain, ISystemFactory system)
         {
             IpTablesRule rule = new IpTablesRule(system);
             rule.GetModuleOrLoad<CoreModule>("core").Jump = chain;
             return rule;
         }
 
-        static IpTablesRule CreateGoto(String chain, ISystemFactory system)
+        public static IpTablesRule CreateGoto(String chain, ISystemFactory system)
         {
             IpTablesRule rule = new IpTablesRule(system);
             rule.GetModuleOrLoad<CoreModule>("core").Goto = chain;
             return rule;
         }
 
-        static IpTablesRule CreateJump(IpTablesChain chain)
+        public static IpTablesRule CreateJump(IpTablesChain chain)
         {
             return CreateJump(chain.Name, chain.System.System);
         }
 
-        static IpTablesRule CreateGoto(IpTablesChain chain)
+        public static IpTablesRule CreateGoto(IpTablesChain chain)
         {
             return CreateGoto(chain.Name, chain.System.System);
         }
