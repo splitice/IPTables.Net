44e9d43546b202faa8730330bec056b27d7e2c51
splitice <mat999@gmail.com>
2014-02-21 19:09:11 +1100

    Added test illustrating a bug in the polyfill module

--- IPTables.Net.Tests/SinglePolyfillTests.cs
+++ IPTables.Net.Tests/SinglePolyfillTests.cs
@@ -17,5 +17,16 @@ namespace IPTables.Net.Tests
 
             Assert.AreEqual(rule, irule.GetFullCommand());
         }
+
+        [Test]
+        public void TestPolyfillParseAdditionalOptionsAfter()
+        {
+            String rule = "-A INPUT -m unknown --unknown -d 1.1.1.1 -p tcp -m tcp --dport 80";
+            IpTablesChainSet chains = new IpTablesChainSet();
+
+            IpTablesRule irule = IpTablesRule.Parse(rule, null, chains);
+
+            Assert.AreEqual(rule, irule.GetFullCommand());
+        }
     }
 }
