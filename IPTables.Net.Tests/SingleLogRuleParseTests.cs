19e70742528430ac727924c574e7873060866a1f
splitice <mat999@gmail.com>
2014-03-18 15:12:39 +1100

    log target tests

---
+++ IPTables.Net.Tests/SingleLogRuleParseTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IPTables.Net.Iptables;
+using NUnit.Framework;
+
+namespace IPTables.Net.Tests
+{
+    [TestFixture]
+    internal class SingleLogRuleParseTests
+    {
+        [Test]
+        public void TestLogWithPrefix()
+        {
+            String rule = "-A INPUT --log-prefix \"IPTABLES (Rule ATTACKED): \" --log-level 7";
+            IpTablesChainSet chains = new IpTablesChainSet();
+
+            IpTablesRule irule = IpTablesRule.Parse(rule, null, chains);
+
+            Assert.AreEqual(rule, irule.GetFullCommand());
+        }
+    }
+}
