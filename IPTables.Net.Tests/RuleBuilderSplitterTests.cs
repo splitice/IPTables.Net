037478b1ac3740270e2d4b5795940ff103ed2d30
splitice <mat999@gmail.com>
2014-10-27 22:31:24 +1100

    Extended splitter tests

--- IPTables.Net.Tests/RuleBuilderSplitterTests.cs
+++ IPTables.Net.Tests/RuleBuilderSplitterTests.cs
@@ -17,7 +17,7 @@ namespace IPTables.Net.Tests
     class RuleBuilderSplitterTests
     {
         [Test]
-        public void Test1()
+        public void TestSplit()
         {
             var mock = new MockIptablesSystemFactory();
             var system = new IpTablesSystem(mock, new MockIpTablesRestoreAdapter());
@@ -35,8 +35,12 @@ namespace IPTables.Net.Tests
             Assert.AreEqual(2, rules.Chains.First().Rules.Count);
             Assert.AreEqual(2, rules.Chains.Skip(1).First().Rules.Count);
             Assert.AreEqual(1, rules.Chains.Skip(2).First().Rules.Count);
-            Assert.AreEqual("-A INPUT -s 8.1.1.1 -j INPUT|8.1.1.1 -m comment --comment '_|RG|INPUT|8.1.1.1'", rules.Chains.First().Rules.First().GetFullCommand());
-            Assert.AreEqual("-A INPUT -s 8.1.1.2 -j INPUT|8.1.1.2 -m comment --comment '_|RG|INPUT|8.1.1.2'", rules.Chains.First().Rules.Skip(1).First().GetFullCommand());
+            Assert.AreEqual("-A INPUT -s 8.1.1.1 -j INPUT|8.1.1.1 -m comment --comment '_|RG|INPUT|8.1.1.1'",
+                rules.Chains.First().Rules.First().GetFullCommand());
+            Assert.AreEqual("-A INPUT -s 8.1.1.2 -j INPUT|8.1.1.2 -m comment --comment '_|RG|INPUT|8.1.1.2'", 
+                rules.Chains.First().Rules.Skip(1).First().GetFullCommand());
+            Assert.AreEqual("-A INPUT|8.1.1.1 -s 8.1.1.1 -j ACCEPT -m udp --sport 1", 
+                rules.Chains.Skip(1).First().Rules.First().GetFullCommand());
         }
 
         private RuleOutputter nestedGenerator(string arg1, string arg2)
