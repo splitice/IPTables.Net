7f609f53e92b6860a3cc4478e282ff6650e399f7
splitice <mat999@gmail.com>
2015-04-07 16:51:57 +1000

    simplify test for travis-ci

--- IPTables.Net.Tests/IptablesLibraryTest.cs
+++ IPTables.Net.Tests/IptablesLibraryTest.cs
@@ -62,12 +62,12 @@ namespace IPTables.Net.Tests
             {
                 IptcInterface iptc = new IptcInterface("filter");
 
-                var status = iptc.ExecuteCommand("iptables -A test2 -d 1.1.1.1 -p tcp -m tcp --dport 80 -j ACCEPT");
+                var status = iptc.ExecuteCommand("iptables -A test2 -d 1.1.1.1 -j ACCEPT");
                 Assert.AreEqual(1, status, "Expected OK return value");
 
                 var rules = iptc.GetRules("test2");
                 Assert.AreEqual(1, rules.Count);
-                Assert.AreEqual("-A test2 -d 1.1.1.1 -p tcp -m tcp --dport 80 -j ACCEPT", iptc.GetRuleString("test2", rules[0]));
+                Assert.AreEqual("-A test2 -d 1.1.1.1/32 -j ACCEPT", iptc.GetRuleString("test2", rules[0]));
             }
         }
     }
