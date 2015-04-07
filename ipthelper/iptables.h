b1be754319c2310992a32a122da8f9975176d924
splitice <mat999@gmail.com>
2015-04-07 15:16:40 +1000

    cleanup excess shared methods

--- ipthelper/iptables.h
+++ ipthelper/iptables.h
@@ -9,15 +9,7 @@
 typedef char xt_chainlabel[32];
 
 /* Your shared library should call one of these. */
-extern int do_command4(int argc, char *argv[], char **table,
+int do_command4(int argc, char *argv[], char **table,
 	void **handle);
-extern int delete_chain4(const xt_chainlabel chain, int verbose,
-			struct xtc_handle *handle);
-extern int flush_entries4(const xt_chainlabel chain, int verbose, 
-			struct xtc_handle *handle);
-extern int for_each_chain4(int (*fn)(const xt_chainlabel, int, struct xtc_handle *),
-		int verbose, int builtinstoo, struct xtc_handle *handle);
-extern void print_rule4(const struct ipt_entry *e,
-		struct xtc_handle *handle, const char *chain, int counters);
 
 #endif /*_IPTABLES_USER_H*/
