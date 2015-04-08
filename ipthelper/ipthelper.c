faef7aaed53a0c1c6208ac7840f7584b2695f24a
splitice <mat999@gmail.com>
2015-04-08 23:31:32 +1000

    Fix target / jump output for new iptables

--- ipthelper/ipthelper.c
+++ ipthelper/ipthelper.c
@@ -542,16 +542,17 @@ extern EXPORT const char* output_rule4(const struct ipt_entry *e, void *h, const
 				}
 			}
 		}
+	}
+
 #ifndef OLD_IPTABLES
-		else if (target_name && (*target_name != '\0')){
+	else if (target_name && (*target_name != '\0')){
 #ifdef IPT_F_GOTO
-			ptr += sprintf(ptr, " -%c %s", e->ip.flags & IPT_F_GOTO ? 'g' : 'j', target_name);
+		ptr += sprintf(ptr, " -%c %s", e->ip.flags & IPT_F_GOTO ? 'g' : 'j', target_name);
 #else
-			ptr += sprintf(ptr, " -j %s", target_name);
-#endif
-		}
+		ptr += sprintf(ptr, " -j %s", target_name);
 #endif
 	}
+#endif
 
 	*ptr = '\0';
 	ptr = buffer;
