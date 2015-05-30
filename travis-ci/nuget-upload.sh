ffda163691d179cda4cc69dfda3ccc0f14dfd600
splitice <mat999@gmail.com>
2015-05-30 17:25:26 +1000

    test (1)

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -7,6 +7,7 @@ DIR=$(dirname "$0")
 
 cd $DIR/../IPTables.Net
 
+ls bin/Release
 mono --runtime=v4.0 ../travis-ci/nuget/NuGet.exe pack IPTables.Net.nuspec -Prop Configuration=Release -BasePath ./
 
 mono --runtime=v4.0 ../travis-ci/nuget/NuGet.exe setApiKey $NUGET_API
