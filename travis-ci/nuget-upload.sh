50bd07dca78ca2d26118e8edbb2eeec051874b53
splitice <mat999@gmail.com>
2015-05-30 15:06:34 +1000

    use newer  mono runtime

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -6,8 +6,8 @@ DIR=$(dirname "$0")
 
 cd $DIR/../IPTables.Net
 
-mono ../.nuget/NuGet.exe pack IPTables.Net.csproj -Prop Configuration=Release
+mono --runtime=v4.0 ../.nuget/NuGet.exe pack IPTables.Net.csproj -Prop Configuration=Release
 
-mono ../.nuget/NuGet.exe setApiKey $NUGET_API
+mono --runtime=v4.0 ../.nuget/NuGet.exe setApiKey $NUGET_API
 
-mono ../.nuget/NuGet.exe push *.nupkg
+mono --runtime=v4.0 ../.nuget/NuGet.exe push *.nupkg
