e9c22d8350d16e8557c67be842e2ab80c685dc2f
splitice <mat999@gmail.com>
2015-05-30 13:45:36 +1000

    set assembly info on all projects

--- travis-ci/autoversion.sh
+++ travis-ci/autoversion.sh
@@ -4,17 +4,22 @@ DIR=$(dirname "$0")
 VERSION=$(git describe --abbrev=0 --tags)
 REVISION=$(git log "$VERSION..HEAD" --oneline | wc -l)
 
+function update_ai {
+	lead='^\/\/ TRAVIS\-CI: START REMOVE$'
+	tail='^\/\/ TRAVIS\-CI: END REMOVE$'
+	C=$(sed -e "/$lead/,/$tail/{ /$lead/{p; r insert_file
+        }; /$tail/p; d }" $f)
+	echo "$C" > $f
+	echo "[assembly: AssemblyVersion(\"$VERSION_STR\")]" >> $f
+	echo "[assembly: AssemblyFileVersion(\"$VERSION_STR\")]" >> $f
+	cat $DIR/../IPTables.Net/Properties/AssemblyInfo.cs
+}
+
 re="([0-9]+\.[0-9]+\.[0-9]+)"
 if [[ $VERSION =~ $re ]]; then
 	VERSION_STR="${BASH_REMATCH[1]}.$REVISION"
 	echo "Version is now: $VERSION_STR"
-	
-	lead='^\/\/ TRAVIS\-CI: START REMOVE$'
-	tail='^\/\/ TRAVIS\-CI: END REMOVE$'
-	C=$(sed -e "/$lead/,/$tail/{ /$lead/{p; r insert_file
-        }; /$tail/p; d }"  $DIR/../IPTables.Net/Properties/AssemblyInfo.cs)
-	echo "$C" > $DIR/../IPTables.Net/Properties/AssemblyInfo.cs
-	echo "[assembly: AssemblyVersion(\"$VERSION_STR\")]" >> $DIR/../IPTables.Net/Properties/AssemblyInfo.cs
-	echo "[assembly: AssemblyFileVersion(\"$VERSION_STR\")]" >> $DIR/../IPTables.Net/Properties/AssemblyInfo.cs
-	cat $DIR/../IPTables.Net/Properties/AssemblyInfo.cs
+	update_ai $DIR/../IPTables.Net/Properties/AssemblyInfo.cs
+	update_ai $DIR/../IPTables.Net.Tests/Properties/AssemblyInfo.cs
+	update_ai $DIR/../IPTables.Net.TestFramework/Properties/AssemblyInfo.cs
 fi
