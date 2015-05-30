d75f60ba7357697fd005dfbfbc6993775e1228b1
splitice <mat999@gmail.com>
2015-05-30 13:37:48 +1000

    add test for version

---
+++ IPTables.Net.Tests/CheckVersion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using IPTables.Net.Iptables;
+using NUnit.Framework;
+
+namespace IPTables.Net.Tests
+{
+    [TestFixture]
+    class CheckVersion
+    {
+        [Test]
+        public void TestVersion()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+
+            Assert.AreNotEqual(version, "1.0.0.0");
+        }
+    }
+}
