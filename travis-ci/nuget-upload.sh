cb189960432f5ec4d72c94c7c1852e5f6838fda6
splitice <mat999@gmail.com>
2015-05-30 14:45:23 +1000

    nuget upload

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+set -e
+
 DIR=$(dirname "$0")
 
 cd $DIR/../IPTables.net
 
 mono ../.nuget/nuget pack IPTables.Net.csproj -Prop Configuration=Release
+
+mono ../.nuget/nuget setApiKey $NUGET_API
+
+mono ../.nuget/nuget push *.nupkg
