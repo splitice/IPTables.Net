eac5e8dc85da29821c23df538e86a86a10291c96
splitice <mat999@gmail.com>
2015-05-30 15:00:35 +1000

    capitalize and add .exe to NuGet

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -6,8 +6,8 @@ DIR=$(dirname "$0")
 
 cd $DIR/../IPTables.Net
 
-mono ../.nuget/nuget pack IPTables.Net.csproj -Prop Configuration=Release
+mono ../.nuget/NuGet.exe pack IPTables.Net.csproj -Prop Configuration=Release
 
-mono ../.nuget/nuget setApiKey $NUGET_API
+mono ../.nuget/NuGet.exe setApiKey $NUGET_API
 
-mono ../.nuget/nuget push *.nupkg
+mono ../.nuget/NuGet.exe push *.nupkg
