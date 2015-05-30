5586b68181448bbc1bfeab8dd23b491a57b09943
splitice <mat999@gmail.com>
2015-05-30 15:48:42 +1000

    mono doesnt support nuget pack on project file (1)

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -x
 set -e
 
 DIR=$(dirname "$0")
 
 cd $DIR/../IPTables.Net
 
-mono --runtime=v4.0 ../.nuget/NuGet.exe pack IPTables.Net.csproj -Prop Configuration=Release
+mono --runtime=v4.0 ../.nuget/NuGet.exe pack IPTables.Net.nuspec -Prop Configuration=Release
 
 mono --runtime=v4.0 ../.nuget/NuGet.exe setApiKey $NUGET_API
 
