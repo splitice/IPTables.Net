aaa015475e9584fa3ac92e20585bcfb4122abc94
splitice <mat999@gmail.com>
2015-05-30 17:16:22 +1000

    silly me its a nuget option not a mono one

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -7,7 +7,7 @@ DIR=$(dirname "$0")
 
 cd $DIR/../IPTables.Net
 
-mono -BasePath ./ --runtime=v4.0 ../travis-ci/nuget/NuGet.exe pack IPTables.Net.nuspec -Prop Configuration=Release
+mono --runtime=v4.0 ../travis-ci/nuget/NuGet.exe -BasePath ./ pack IPTables.Net.nuspec -Prop Configuration=Release
 
 mono --runtime=v4.0 ../travis-ci/nuget/NuGet.exe setApiKey $NUGET_API
 
