facf7046d53180ac9b378a0b06254485bf4dd322
splitice <mat999@gmail.com>
2015-05-31 14:05:55 +1000

    updated nuget-upload.sh

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -7,7 +7,6 @@ P=$DIR/../$1
 
 cd $P
 
-ls bin/Release
-mono --runtime=v4.0 ../.nuget/NuGet.exe pack IPTables.Net.nuspec -Prop Configuration=Release -BasePath $P
+mono --runtime=v4.0 ../.nuget/NuGet.exe pack *.nuspec -Prop Configuration=Release -BasePath $P
 
 mono --runtime=v4.0 ../.nuget/NuGet.exe push *.nupkg -ApiKey $NUGET_API
