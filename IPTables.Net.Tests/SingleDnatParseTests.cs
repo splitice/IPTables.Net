3555132802897fd2ed85d813c0e76541f51fd35a
splitice <mat999@gmail.com>
2015-08-08 17:58:21 +1000

    More robust comparison

---
+++ IPTables.Net.Tests/SingleDnatParseTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using IPTables.Net.Iptables;
+using IPTables.Net.Iptables.Modules.Comment;
+using NUnit.Framework;
+
+namespace IPTables.Net.Tests
+{
+    [TestFixture]
+    internal class SingleDnatParseTests
+    {
+        [Test]
+        public void DnatTest1()
+        {
+            String rule = "-A A+B -p tcp -j DNAT --to-destination 1.2.3.4";
+            IpTablesChainSet chains = new IpTablesChainSet(4);
+
+            IpTablesRule irule = IpTablesRule.Parse(rule, null, chains, 4);
+
+            Assert.AreEqual(rule, irule.GetActionCommand());
+            Assert.AreEqual(irule, IpTablesRule.Parse(rule, null, chains, 4));
+        }
+
+    }
+}
