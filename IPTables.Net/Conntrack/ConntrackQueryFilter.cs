15d0238d5486dd7860fb812c1e15203c2f8476ca
splitice <mat999@gmail.com>
2016-03-01 07:59:47 +1100

    explicit structure layout and size

--- IPTables.Net/Conntrack/ConntrackQueryFilter.cs
+++ IPTables.Net/Conntrack/ConntrackQueryFilter.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace IPTables.Net.Conntrack
 {
+    [StructLayout(LayoutKind.Sequential, Pack=4, Size=16)]
     public struct ConntrackQueryFilter
     {
         public UInt16 Key;
