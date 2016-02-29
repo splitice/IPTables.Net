876b17e44410aa98a4f2aafa7da292de61e3e3a4
splitice <mat999@gmail.com>
2016-02-29 22:57:23 +1100

    compile debug version of iptch for more information

--- ipthelper/install.sh
+++ ipthelper/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-make ADDITIONAL_CFLAGS=$(printenv ADDITIONAL_CFLAGS) CXX=$(printenv CXX) CONFIG="Release" 
+CONFIG=$(printenv CONFIG)
+if [[ -z "$CONFIG" ]]; then
+	CONFIG="Release"
+fi
+
+make ADDITIONAL_CFLAGS=$(printenv ADDITIONAL_CFLAGS) CXX=$(printenv CXX) CONFIG="$CONFIG" 
 cp Release/libipthelper.so /usr/lib/
 ldconfig -n /usr/lib
 
