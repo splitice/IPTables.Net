6e2afd99846f3d66476063df26a65bc06b003ade
splitice <mat999@gmail.com>
2016-08-21 22:05:34 +1000

    add posix ip family helper

---
+++ IPTables.Net/Iptables/Helpers/PosixFamilyHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTables.Net.Iptables.Helpers
+{
+    public class PosixFamilyHelpers
+    {
+        public String GetIpFamily(int ipVersion)
+        {
+            switch (ipVersion)
+            {
+                case 4:
+                    return "inet";
+
+                case 6:
+                    return "inet6";
+            }
+
+            throw new InvalidOperationException("Invalid IP Version");
+        }
+    }
+}
