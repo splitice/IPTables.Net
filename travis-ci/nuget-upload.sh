b88113a160b7069e9a79b29d45f44afb9a0665ce
splitice <mat999@gmail.com>
2016-09-07 21:19:45 +1000

    dont deploy PRs

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -7,6 +7,8 @@ P=$DIR/../$1
 
 cd $P
 
-mono --runtime=v4.0 ../.nuget/NuGet.exe pack *.nuspec -Prop Configuration=Release -BasePath $P
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+	mono --runtime=v4.0 ../.nuget/NuGet.exe pack *.nuspec -Prop Configuration=Release -BasePath $P
 
-mono --runtime=v4.0 ../.nuget/NuGet.exe push *.nupkg -ApiKey $NUGET_API
+	mono --runtime=v4.0 ../.nuget/NuGet.exe push *.nupkg -ApiKey $NUGET_API
+fi
