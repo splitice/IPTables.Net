b9726f2df27b3be3f55ee9935faaf51d08c5cad5
splitice <mat999@gmail.com>
2016-10-09 18:11:26 +1100

    add nfacct tests

---
+++ IPTables.Net.Tests/SingleNfacctRuleParseTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using IPTables.Net.Iptables;
+using NUnit.Framework;
+
+namespace IPTables.Net.Tests
+{
+    [TestFixture]
+    internal class SingleNfacctRuleParseTests
+    {
+        [Test]
+        public void TestSmall()
+        {
+            String rule = "-A INPUT -j ACCEPT -m nfacct --nfacct-name test";
+            IpTablesChainSet chains = new IpTablesChainSet(4);
+
+            IpTablesRule irule = IpTablesRule.Parse(rule, null, chains, 4);
+
+            Assert.AreEqual(rule, irule.GetActionCommand());
+        }
+
+        [Test]
+        public void TestQuote()
+        {
+            String rule = "-A INPUT -j ACCEPT -m nfacct --nfacct-name \"test\"";
+            String rule2 = "-A INPUT -j ACCEPT -m nfacct --nfacct-name test";
+            IpTablesChainSet chains = new IpTablesChainSet(4);
+
+            IpTablesRule irule = IpTablesRule.Parse(rule, null, chains, 4);
+
+            Assert.AreEqual(rule2, irule.GetActionCommand());
+            Assert.AreEqual(IpTablesRule.Parse(rule2, null, chains, 4), irule);
+        }
+
+        [Test]
+        public void TestDoubleSpace()
+        {
+            String rule = "-A INPUT -j ACCEPT -m nfacct --nfacct-name  \"test\"";
+            String rule2 = "-A INPUT -j ACCEPT -m nfacct --nfacct-name test";
+            IpTablesChainSet chains = new IpTablesChainSet(4);
+
+            IpTablesRule irule = IpTablesRule.Parse(rule, null, chains, 4);
+
+            Assert.AreEqual(rule2, irule.GetActionCommand());
+            Assert.AreEqual(IpTablesRule.Parse(rule2, null, chains, 4), irule);
+        }
+    }
+}
