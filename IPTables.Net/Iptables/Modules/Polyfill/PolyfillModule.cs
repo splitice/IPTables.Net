4dd738e248a537197bfe0717b95271cd213b7f51
splitice <mat999@gmail.com>
2017-01-17 22:17:05 +1100

    polyfil not as dictionary

--- IPTables.Net/Iptables/Modules/Polyfill/PolyfillModule.cs
+++ IPTables.Net/Iptables/Modules/Polyfill/PolyfillModule.cs
@@ -9,6 +9,8 @@ namespace IPTables.Net.Iptables.Modules.Polyfill
     public class PolyfillModule : ModuleBase, IIpTablesModule, IEquatable<PolyfillModule>
     {
         private readonly Dictionary<String, List<String>> _data = new Dictionary<String, List<String>>();
+        private readonly Dictionary<String, bool> _not = new Dictionary<String, bool>();
+        
 
         public PolyfillModule(int version) : base(version)
         {
@@ -30,11 +32,8 @@ namespace IPTables.Net.Iptables.Modules.Polyfill
         public int Feed(RuleParser parser, bool not)
         {
             String current = parser.GetCurrentArg();
-            _data.Add(current, new List<string>());
-            if (not)
-            {
-                _data[current].Add("!");
-            }
+            _data.Add(current, new List<string> { current });
+            _not[current] = not;
             for (int i = 1; i < parser.GetRemainingArgs(); i++)
             {
                 string arg = parser.GetNextArg(i);
@@ -53,6 +52,10 @@ namespace IPTables.Net.Iptables.Modules.Polyfill
 
             foreach (var pair in _data)
             {
+                if (_not[pair.Key])
+                {
+                    sb.Append("! ");
+                }
                 sb.Append(pair.Key);
                 sb.Append(" ");
                 foreach (string a in pair.Value)
