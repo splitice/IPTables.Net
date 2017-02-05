40de4524568d35865e8bd6cecfa7cd8dc8f4d3a6
splitice <mat999@gmail.com>
2017-02-05 13:06:57 +1100

    add statistic test

---
+++ IPTables.Net.Tests/SingleStatisticParseTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IPTables.Net.Iptables;
+using NUnit.Framework;
+
+namespace IPTables.Net.Tests
+{
+    [TestFixture]
+    internal class SingleStatisticParseTests
+    {
+        [Test]
+        public void Test1()
+        {
+            String rule = "-A FORWARD -m statistic --mode nth --every 3 --packet 1";
+            IpTablesChainSet chains = new IpTablesChainSet(4);
+
+            IpTablesRule irule = IpTablesRule.Parse(rule, null, chains, 4);
+
+            Assert.AreEqual(rule, irule.GetActionCommand());
+        }
+    }
+}
