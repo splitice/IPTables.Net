1534abb991bbae929adaf6393aeaee43991e3d56
splitice <mheard@x4b.net>
2017-10-01 18:24:15 +1100

    add source parameter

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -13,5 +13,5 @@ sudo chmod 0777 /tmp/NuGetScratch -R
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	nuget pack *.nuspec -Prop Configuration=Release -BasePath $P
 
-	nuget push *.nupkg -ApiKey $NUGET_API -Verbosity detailed
+	nuget push *.nupkg -ApiKey $NUGET_API -Source https://www.nuget.org/api/v2/package
 fi
