35c791437e0a50dd901fdbce884cad4c9654e91c
splitice <mheard@x4b.net>
2017-10-01 18:01:36 +1100

    detailed verbosity

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -10,5 +10,5 @@ cd $P
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	nuget pack *.nuspec -Prop Configuration=Release -BasePath $P
 
-	nuget push *.nupkg -ApiKey $NUGET_API
+	nuget push *.nupkg -ApiKey $NUGET_API -Verbosity detailed
 fi
