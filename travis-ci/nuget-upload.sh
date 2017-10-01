cc5176613eccede15175ff4177c6c9581994df94
splitice <mheard@x4b.net>
2017-10-01 18:17:58 +1100

    chmod nuget scratch

--- travis-ci/nuget-upload.sh
+++ travis-ci/nuget-upload.sh
@@ -8,6 +8,7 @@ P=$DIR/../$1
 cd $P
 
 sudo nuget update -self
+sudo chmod 0777 /tmp/NuGetScratch -R
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	nuget pack *.nuspec -Prop Configuration=Release -BasePath $P
