3449c23b9483665dae684f391efab3629064ddc0
Mathew Heard <splitice@users.noreply.github.com>
2020-05-10 14:20:03 +1000

    ...

--- travis-ci/test-and-publish.sh
+++ travis-ci/test-and-publish.sh
@@ -8,4 +8,15 @@ bash ./ipthelper/test/build.sh
 # ./ipthelper/test/main
 
 sudo nunit-console -framework=4.0 ./IPTables.Net.Tests/bin/Debug/IPTables.Net.Tests.dll -exclude Integration,NotWorkingOnMono,NotWorkingOnTravis
-travis_retry bash travis-ci/nuget-upload.sh IPTables.Net
+
+set +e
+
+n=0
+until [ "$n" -ge 5 ]
+do
+    bash travis-ci/nuget-upload.sh IPTables.Net
+    if [[ $? == "0" ]]; then
+        exit 0
+    fi
+done
+exit 1
