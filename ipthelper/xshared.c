9a8dccbb9eb2c3a2ffbe44696020c42d553c1bec
Mathew Heard <splitice@users.noreply.github.com>
2020-05-16 16:26:45 +1000

    ...

--- ipthelper/xshared.c
+++ ipthelper/xshared.c
@@ -257,24 +257,6 @@ void xtables_unlock(int lock)
 
 int xtables_lock_or_exit(int wait, struct timeval *wait_interval)
 {
-	int lock = xtables_lock(wait, wait_interval);
-
-	if (lock == XT_LOCK_FAILED) {
-		xtables_free_opts(1);
-		exit(RESOURCE_PROBLEM);
-	}
-
-	if (lock == XT_LOCK_BUSY) {
-		fprintf(stderr, "Another app is currently holding the xtables lock. ");
-		if (wait == 0)
-			fprintf(stderr, "Perhaps you want to use the -w option?\n");
-		else
-			fprintf(stderr, "Stopped waiting after %ds.\n", wait);
-		xtables_free_opts(1);
-		exit(RESOURCE_PROBLEM);
-	}
-
-	return lock;
 }
 
 int parse_wait_time(int argc, char *argv[])
