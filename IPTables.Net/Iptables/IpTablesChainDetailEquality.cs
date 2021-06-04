cfc6f4f5463336bf30012ef548bc6d7383e00b5d
Mathew Heard <splitice@users.noreply.github.com>
2021-06-04 22:10:00 +1000

    add IpTablesChainDetailEquality

---
+++ IPTables.Net/Iptables/IpTablesChainDetailEquality.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPTables.Net.Iptables
+{
+    public class IpTablesChainDetailEquality: IEqualityComparer<IpTablesChain>
+    {
+        public bool Equals(IpTablesChain x, IpTablesChain y)
+        {
+            if (x == y) return true;
+            if (x == null || y == null) return false;
+            return x.Name == y.Name && x.Table == y.Table;
+        }
+
+        public int GetHashCode(IpTablesChain obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
