d16f3d6d6a9e7657be253f98f4c5f14aaf029941
Mathew Heard <splitice@users.noreply.github.com>
2022-03-24 22:22:34 +1100

    improve shell escaping and add tests

--- IPTables.Net.Tests/EscapeHelperTests.cs
+++ IPTables.Net.Tests/EscapeHelperTests.cs
@@ -25,5 +25,14 @@ namespace IPTables.Net.Tests
             Assert.AreEqual("'a|word'", ShellHelper.EscapeArguments("a|word"));
             Assert.AreEqual("singleword", ShellHelper.EscapeArguments("singleword"));
         }
+        [Test]
+        public void TestSpace()
+        {
+            Assert.AreEqual("a word", ShellHelper.BuildArgumentString(new []{"a", "word"}));
+            Assert.AreEqual("\"two words\"", ShellHelper.BuildArgumentString(new[] { "two words" }));
+            Assert.AreEqual("\"two words and \\\"punctuation\\\"\"", ShellHelper.BuildArgumentString(new[] { "two words and \"punctuation\"" }));
+            Assert.AreEqual("bash -c \"bash -c \\\"echo a\\\"\"", ShellHelper.BuildArgumentString(new[] { "bash", "-c", ShellHelper.BuildArgumentString(new []{"bash", "-c", "echo a"}) }));
+            Assert.AreEqual("bash -c \"bash -c \\\"echo \\\\\\\"a\\\\\\\"\\\"\"", ShellHelper.BuildArgumentString(new[] { "bash", "-c", ShellHelper.BuildArgumentString(new[] { "bash", "-c", "echo \"a\"" }) }));
+        }
     }
 }
--- IPTables.Net/Supporting/ShellHelper.cs
+++ IPTables.Net/Supporting/ShellHelper.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace IPTables.Net.Supporting
 {
@@ -31,82 +32,33 @@ namespace IPTables.Net.Supporting
         /// Undo the processing which took place to create string[] args in Main, so that the next process will
         /// receive the same string[] args.
         /// </summary>
-        /// <remarks>
-        /// See https://blogs.msdn.microsoft.com/twistylittlepassagesallalike/2011/04/23/everyone-quotes-command-line-arguments-the-wrong-way/
-        /// </remarks>
         /// <param name="args">The arguments</param>
         /// <returns>A single string of escaped arguments</returns>
-        public static string BuildArgumentString(IEnumerable<string> args)
-            => string.Join(" ", args.Select(EscapeSingleArg));
-
-        private static string EscapeSingleArg(string arg)
+        public static string BuildArgumentString(params string[] args)
         {
-            var sb = new StringBuilder();
-
-            var needsQuotes = arg.Length == 0 || ContainsWhitespace(arg);
-            var isQuoted = needsQuotes || IsSurroundedWithQuotes(arg);
-
-            if (needsQuotes)
+            StringBuilder arguments = new StringBuilder();
+            Regex invalidChar = new Regex("[\x00\x0a\x0d]");//  these can not be escaped
+            Regex needsQuotes = new Regex(@"\s|""");//          contains whitespace or two quote characters
+            Regex escapeQuote = new Regex(@"(\\*)(""|$)");//    one or more '\' followed with a quote or end of string
+            for (int carg = 0; args != null && carg < args.Length; carg++)
             {
-                sb.Append('"');
-            }
-
-            for (var i = 0; i < arg.Length; ++i)
-            {
-                var backslashes = 0;
-
-                // Consume all backslashes
-                while (i < arg.Length && arg[i] == '\\')
-                {
-                    backslashes++;
-                    i++;
-                }
-
-                if (i == arg.Length && isQuoted)
-                {
-                    // Escape any backslashes at the end of the arg when the argument is also quoted.
-                    // This ensures the outside quote is interpreted as an argument delimiter
-                    sb.Append('\\', 2 * backslashes);
-                }
-                else if (i == arg.Length)
-                {
-                    // At then end of the arg, which isn't quoted,
-                    // just add the backslashes, no need to escape
-                    sb.Append('\\', backslashes);
-                }
-                else if (arg[i] == '"')
-                {
-                    // Escape any preceding backslashes and the quote
-                    sb.Append('\\', (2 * backslashes) + 1);
-                    sb.Append('"');
-                }
+                if (args[carg] == null) { throw new ArgumentNullException("args[" + carg + "]"); }
+                if (invalidChar.IsMatch(args[carg])) { throw new ArgumentOutOfRangeException("args[" + carg + "]"); }
+                if (args[carg] == String.Empty) { arguments.Append("\"\""); }
+                else if (!needsQuotes.IsMatch(args[carg])) { arguments.Append(args[carg]); }
                 else
                 {
-                    // Output any consumed backslashes and the character
-                    sb.Append('\\', backslashes);
-                    sb.Append(arg[i]);
+                    arguments.Append('"');
+                    arguments.Append(escapeQuote.Replace(args[carg], m =>
+                        m.Groups[1].Value + m.Groups[1].Value +
+                        (m.Groups[2].Value == "\"" ? "\\\"" : "")
+                    ));
+                    arguments.Append('"');
                 }
+                if (carg + 1 < args.Length)
+                    arguments.Append(' ');
             }
-
-            if (needsQuotes)
-            {
-                sb.Append('"');
-            }
-
-            return sb.ToString();
+            return arguments.ToString();
         }
-
-        private static bool IsSurroundedWithQuotes(string argument)
-        {
-            if (argument.Length <= 1)
-            {
-                return false;
-            }
-
-            return argument[0] == '"' && argument[argument.Length - 1] == '"';
-        }
-
-        private static bool ContainsWhitespace(string argument)
-            => argument.IndexOfAny(new[] { ' ', '\t', '\n' }) >= 0;
     }
 }
